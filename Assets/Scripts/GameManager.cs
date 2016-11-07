f2d3b86a76261da178bd9b801cc840fa086e3f32
victor martinez simon <victormartinezsimon@gmail.com>
2016-11-07 19:15:12 +0100

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+
+  public Transform InitialPosition;
+
+  public GameObject[] Pieces;
+
+  public float TimeAcum = 0;
+
+  // Use this for initialization
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    TimeAcum += Time.deltaTime;
+    if (TimeAcum > 3)
+    {
+      GeneratePiece();
+      TimeAcum = 0;
+    }
+  }
+
+  private void GeneratePiece()
+  {
+    GameObject piece = Pieces[Random.Range(0, Pieces.Length)];
+    GameObject go = Instantiate(piece, InitialPosition.position, Quaternion.identity) as GameObject;
+    go.transform.rotation = Quaternion.Euler(0, 0, 90 * Random.Range(0,4));
+  }
+}
---
+++ Assets/Scripts/PiezeManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PiezeManager : MonoBehaviour {
+
+  public GameObject[] piezes;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+}
