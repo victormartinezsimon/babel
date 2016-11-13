b3ce9ad6657506cd35038ed2e4b6221734aa0149
victor martinez simon <victormartinezsimon@gmail.com>
2016-11-13 13:21:20 +0100

    add a helper to all the pieces

---
+++ Assets/Scripts/Pieces/PieceHelper.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PieceHelper : MonoBehaviour {
+
+  public float MinX = -6;
+  public float MaxX = 6;
+  private Renderer m_renderer;
+
+  void Start()
+  {
+    m_renderer = GetComponent<Renderer>();
+  }
+	
+	// Update is called once per frame
+	void Update () {
+    float x = transform.position.x;
+	  if( x < MinX || x > MaxX)
+    {
+      m_renderer.material.color = Color.red;
+    }
+    else
+    {
+      m_renderer.material.color = Color.white;
+    }
+	}
+}
