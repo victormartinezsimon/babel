6ea5b5e4a460e723cc31ac5cd65c9fbc87f57ec5
victor martinez simon <victormartinezsimon@gmail.com>
2016-11-19 19:57:03 +0100

    add forward and rewind

--- Assets/Scripts/Remember/RememberManager.cs
+++ Assets/Scripts/Remember/RememberManager.cs
@@ -6,22 +6,28 @@ public class RememberManager : MonoBehaviour
 {
   public int MaxSteps;
   public float TimeBetweenStep;
-  public enum PlayingMode { RECORDING, PLAY, REWIND };
-  private PlayingMode m_state;
+  public enum PlayingMode { RECORDING, FORWARD, REWIND, NONE };
+  public PlayingMode m_state;
 
-  public struct Actions
+  public struct Action
   {
-    public enum Action { CREATE, DESTROY, POSITION }
-    public Transform m_transform;
-    public Action m_action;
+    public Vector3 m_position;
+    public GameObject go;
     public int ID;
   }
 
-  public Dictionary<float, List<Actions>> m_allActions;
-  private Dictionary<int, GameObject> m_gameobjects;
-  private int m_lastId;
-  private float m_totalTime;
-  private float m_timeSnapShot;
+  public Dictionary<float, List<Action>> m_allActions;
+  //private Dictionary<int, GameObject> m_gameobjectsForSnapshots;
+  private int m_gameobjectID;
+  private float m_tickInRecord;
+  private float m_timeWaitSnapShot;
+
+  //animation stuff
+  private float m_timeAcumAnimation;
+  private List<float> m_timesSnapShots;
+  public float FactorRewind = 1;
+  public float FactorForward;
+  private Dictionary<int, GameObject> m_gameobjectsForAnimations;
 
   #region singleton
   private static RememberManager m_instance;
@@ -39,9 +45,11 @@ public class RememberManager : MonoBehaviour
       return;
     }
     m_instance = this;
-    m_allActions = new Dictionary<float, List<Actions>>();
-    m_gameobjects = new Dictionary<int, GameObject>();
-    m_lastId = 0;
+    m_allActions = new Dictionary<float, List<Action>>();
+    m_timesSnapShots = new List<float>();
+    m_gameobjectsForAnimations = new Dictionary<int, GameObject>();
+    m_gameobjectID = 0;
+    m_timeWaitSnapShot = 0;
     m_state = PlayingMode.RECORDING;
   }
   void Update()
@@ -49,6 +57,8 @@ public class RememberManager : MonoBehaviour
     switch (m_state)
     {
       case PlayingMode.RECORDING: Record(); break;
+      case PlayingMode.REWIND: Rewind(); break;
+      case PlayingMode.FORWARD: Forward(); break;
     }
   }
 
@@ -56,44 +66,49 @@ public class RememberManager : MonoBehaviour
   {
     CreateSnapShot();
 
-    Actions a = new Actions();
-    a.ID = m_lastId;
-    a.m_action = Actions.Action.CREATE;
-    a.m_transform = go.transform;
+    Action a = new Action();
+    a.ID = m_gameobjectID;
+    a.m_position = go.transform.position;
+    a.go = go;
 
-    m_allActions[m_totalTime].Add(a);
-    m_gameobjects.Add(m_lastId, go);
+    m_allActions[m_tickInRecord].Add(a);
 
-    ++m_lastId;
-    return m_lastId;
+    ++m_gameobjectID;
+    return a.ID;
   }
   public void Destroy(int id)
   {
-    GameObject go = m_gameobjects[id];
-    m_gameobjects.Remove(id);
     CreateSnapShot();
 
-    Actions aDestroy = new Actions();
-    aDestroy.ID = id;
-    aDestroy.m_action = Actions.Action.DESTROY;
-    aDestroy.m_transform = go.transform;
-
-    m_allActions[m_totalTime].Add(aDestroy);
+    List<Action> list = m_allActions[m_timesSnapShots[m_timesSnapShots.Count - 1]];
+    Action a = list.Find((a1) => a1.ID == id);
+    list.Remove(a);
+    m_allActions[m_timesSnapShots[m_timesSnapShots.Count - 1]] = list;
   }
   private void CreateSnapShot()
   {
     RemoveFirstSnapShot();
-
-    List<Actions> list = new List<Actions>();
-    foreach (KeyValuePair<int, GameObject> par in m_gameobjects)
+    if (m_timesSnapShots.Count > 0)
     {
-      Actions a = new Actions();
-      a.m_action = Actions.Action.POSITION;
-      a.ID = par.Key;
-      a.m_transform = par.Value.transform;
-      list.Add(a);
+      List<Action> list = new List<Action>();
+      List<Action> lastActions = m_allActions[m_timesSnapShots[m_timesSnapShots.Count - 1]];
+      for (int i = 0; i < lastActions.Count; ++i)
+      {
+        Action action = lastActions[i];
+        Action a = new Action();
+        a.ID = action.ID;
+        a.m_position = action.go.transform.position;
+        a.go = action.go;
+        list.Add(a);
+      }
+      m_allActions.Add(m_tickInRecord, list);
     }
-    m_allActions.Add(m_totalTime, list);
+    else
+    {
+      m_allActions.Add(m_tickInRecord, new List<Action>());
+    }
+
+    m_timesSnapShots.Add(m_tickInRecord);
   }
   private void RemoveFirstSnapShot()
   {
@@ -102,24 +117,136 @@ public class RememberManager : MonoBehaviour
       return;
     }
 
-    float min = float.MaxValue;
-    foreach (float value in m_allActions.Keys)
-    {
-      min = Mathf.Min(min, value);
-    }
-    m_allActions.Remove(min);
+    float timeToDelete = m_timesSnapShots[0];
+    m_allActions.Remove(timeToDelete);
+    m_timesSnapShots.RemoveAt(0);
   }
 
   public void Record()
   {
-    m_totalTime += Time.deltaTime;
-    m_timeSnapShot += Time.deltaTime;
+    m_tickInRecord += Time.deltaTime;
+    m_timeWaitSnapShot += Time.deltaTime;
 
-    if (m_timeSnapShot >= TimeBetweenStep)
+    if (m_timeWaitSnapShot >= TimeBetweenStep)
     {
-      m_timeSnapShot = 0;
+      m_timeWaitSnapShot = 0;
       CreateSnapShot();
     }
   }
 
+  [ContextMenu("Rewind")]
+  public void DoRewind()
+  {
+    m_timeAcumAnimation = m_timesSnapShots[m_timesSnapShots.Count - 1];
+    CloneAllGameObjects();
+    ColocateAllPiecesAnimation(m_timesSnapShots.Count - 2, m_timeAcumAnimation);
+    HideOriginalGameObjects();
+    m_state = PlayingMode.REWIND;
+  }
+  [ContextMenu("Forward")]
+  public void DoForward()
+  {
+    m_timeAcumAnimation = m_timesSnapShots[0];
+    CloneAllGameObjects();
+    ColocateAllPiecesAnimation(0, 0);
+    HideOriginalGameObjects();
+    m_state = PlayingMode.FORWARD;
+  }
+
+  private void CloneAllGameObjects()
+  {
+    foreach (KeyValuePair<int, GameObject> par in m_gameobjectsForAnimations)
+    {
+      Destroy(par.Value);
+    }
+    m_gameobjectsForAnimations.Clear();
+
+    List<Action> m_list = m_allActions[m_timesSnapShots[m_timesSnapShots.Count - 1]];
+    for (int i = 0; i < m_list.Count; ++i)
+    {
+      m_gameobjectsForAnimations.Add(m_list[i].ID, Instantiate(m_list[i].go) as GameObject);
+    }
+  }
+  private void HideOriginalGameObjects()
+  {
+    List<Action> m_list = m_allActions[m_timesSnapShots[m_timesSnapShots.Count - 1]];
+    for (int i = 0; i < m_list.Count; ++i)
+    {
+      m_list[i].go.SetActive(false);
+    }
+  }
+
+  public void Rewind()
+  {
+    if (m_timeAcumAnimation > m_timesSnapShots[0])
+    {
+      m_timeAcumAnimation -= Time.deltaTime * FactorRewind;
+      int index = FindCloseIndex(m_timeAcumAnimation);
+      float timeLerp = (m_timeAcumAnimation - m_timesSnapShots[index]) / (m_timesSnapShots[index + 1] - m_timesSnapShots[index]);
+      ColocateAllPiecesAnimation(index, timeLerp);
+    }
+    else
+    {
+      DoForward();
+    }
+  }
+
+  public void Forward()
+  {
+    if (m_timeAcumAnimation < m_timesSnapShots[m_timesSnapShots.Count - 1])
+    {
+      m_timeAcumAnimation += Time.deltaTime * FactorForward;
+      int index = FindCloseIndex(m_timeAcumAnimation);
+      float timeLerp = (m_timeAcumAnimation - m_timesSnapShots[index]) / (m_timesSnapShots[index + 1] - m_timesSnapShots[index]);
+      ColocateAllPiecesAnimation(index, timeLerp);
+    }
+    else
+    {
+      DoRewind();
+    }
+  }
+
+  private int FindCloseIndex(float time)
+  {
+    for (int i = 1; i < m_timesSnapShots.Count; ++i)
+    {
+      if (m_timesSnapShots[i] >= time)
+      {
+        return i - 1;
+      }
+    }
+    return 0;
+  }
+
+  private void ColocateAllPiecesAnimation(int index, float timeForLerp)
+  {
+    float timeFromSnapshot = m_timesSnapShots[index];
+    float timeFromSnapshotNext = m_timesSnapShots[index + 1];
+    List<Action> actionsInIndex = m_allActions[timeFromSnapshot];
+    List<Action> actionsInNextIndex = m_allActions[timeFromSnapshotNext];
+
+    for (int i = 0; i < actionsInIndex.Count; ++i)
+    {
+      Action actual = actionsInIndex[i];
+      Action next = actionsInNextIndex.Find((a) => a.ID == actual.ID);
+      if (!m_gameobjectsForAnimations.ContainsKey(actual.ID))
+      {
+        m_gameobjectsForAnimations.Add(actual.ID, Instantiate(actual.go) as GameObject);
+      }
+
+      GameObject go = m_gameobjectsForAnimations[actual.ID];
+      Vector3 position = Vector3.Lerp(actual.m_position, next.m_position, timeForLerp);
+      go.transform.position = position;
+
+    }
+
+    foreach (int key in m_gameobjectsForAnimations.Keys)
+    {
+      if (!actionsInIndex.Exists((a) => a.ID == key))
+      {
+        Destroy(m_gameobjectsForAnimations[key]);
+        m_gameobjectsForAnimations.Remove(key);
+      }
+    }
+  }
 }
